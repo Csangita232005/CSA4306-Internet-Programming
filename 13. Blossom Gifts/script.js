05b4eec472b4207b70f914aeb4a57f861db12b25
Csangita232005 <csangita0108@gmail.com>
2026-08-20 08:42:58 +0530

    Add files via upload

---
+++ 13. Blossom Gifts/script.js
@@ -0,0 +1,8 @@
+function addCart(item) {
+    document.getElementById("cartItem").innerHTML =
+        item + " added to cart.";
+}
+
+function checkout() {
+    alert("Order placed successfully!");
+}
